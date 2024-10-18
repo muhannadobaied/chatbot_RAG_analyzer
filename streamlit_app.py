3f2471a46a88a03e10285fafc97ce756d1426d62
Muhannad obaied <64822904+muhannadobaied@users.noreply.github.com>
2024-10-18 18:42:35 +0000

    testipaddress

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,10 +29,10 @@ if prompt := st.chat_input():
     if not company_name:
         st.info("Please add your company name to continue.")
         st.stop()
-    # tools = [cat_facts]
-    # client = Client(host="https://verifyaisy.com/ollama/")
-    # print("MMMMMM", st.session_state.messages)
-    # response =client.chat(model="llama3.2:latest", messages=[{"role": "user", "content": st.session_state.messages}], stream=True)
+    tools = [cat_facts]
+    client = Client(host="http://109.199.116.46/")
+    print("MMMMMM", st.session_state.messages)
+    response =client.chat(model="llama3.2:latest", messages=[{"role": "user", "content": st.session_state.messages}], stream=True)
     # model = ChatOllama(model="llama3.2:latest", base_url="https://verifyaisy.com/ollama/").bind_tools(tools=tools)
     # prompt = hub.pull("hwchase17/openai-tools-agent")
     # agent = create_tool_calling_agent(model, tools, prompt)
@@ -42,31 +42,31 @@ if prompt := st.chat_input():
     # response = client.invoke(input={
     #     "input" : st.session_state.messages
     # })
-    # print("RESPONSE", response)
-    # full_answer = ''
-    # for chunk in response:
-    #     print(chunk['message']['content'], end='', flush=True)
-    #     full_answer =''.join([full_answer,chunk['message']['content']])
+    print("RESPONSE", response)
+    full_answer = ''
+    for chunk in response:
+        print(chunk['message']['content'], end='', flush=True)
+        full_answer =''.join([full_answer,chunk['message']['content']])
 
-    # msg = full_answer
-    # msg = response
-    # st.session_state.messages.append({"role": "assistant", "content": msg})
-    # st.chat_message("assistant").write(msg)
+    msg = full_answer
+    msg = response
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
 
 
-    url = "https://verifyaisy.com/ollama/api/chat"
-    headers = {
-        "Authorization": "AAAAC3NzaC1lZDI1NTE5AAAAIG5PyAx3VlbI8441XShYE7BPHb2DA+b2D2n8Ku6PPaWx",  # If your API requires a key
-        "Content-Type": "application/json",
-    }
+    # url = "http://109.199.116.46/"
+    # headers = {
+    #     "Authorization": "AAAAC3NzaC1lZDI1NTE5AAAAIG5PyAx3VlbI8441XShYE7BPHb2DA+b2D2n8Ku6PPaWx",  # If your API requires a key
+    #     "Content-Type": "application/json",
+    # }
 
-    data = {
-        "messages": [{"role": "user", "content": "Hello!"}]
-    }
+    # data = {
+    #     "messages": [{"role": "user", "content": "Hello!"}]
+    # }
 
-    response = requests.post(url, headers=headers, json=data)
-    st.session_state.messages.append({"role": "assistant", "content": response})
-    if response.status_code == 200:
-        print("Success:", response.json())
-    else:
-        print("Error:", response.status_code, response.text)
+    # response = requests.post(url, headers=headers, json=data)
+    # st.session_state.messages.append({"role": "assistant", "content": response})
+    # if response.status_code == 200:
+    #     print("Success:", response.json())
+    # else:
+    #     print("Error:", response.status_code, response.text)
